8d8a4d2b7efbd938a543bb5e425cdae8c3f4f06c
ronykris <rony.kris@gmail.com>
2023-09-29 13:16:41 +0530

    update

--- frontend/components/Graphs/NodeChart.tsx
+++ frontend/components/Graphs/NodeChart.tsx
@@ -29,7 +29,7 @@ export default function FocusGraph() {
       //gData = data
     }
     useEffect(() => {   
-      fetchNodes(500)                    
+      fetchNodes(5000)                    
         setTimeout(() => {          
           setLoaded(true)
         }, 1000);
