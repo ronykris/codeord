7cf1500e73870a8ec99cbb6e6d5e851bd51f2f38
ronykris <rony.kris@gmail.com>
2023-10-01 05:19:53 +0530

    update

--- frontend/components/Graphs/NodeChart.tsx
+++ frontend/components/Graphs/NodeChart.tsx
@@ -1,19 +1,6 @@
 import ForceGraph3D,  { ForceGraphMethods, GraphData } from 'react-force-graph-3d';
 import React, { useCallback, useEffect, useRef, useState } from "react";
 
-// Random data
-/*
-const N = 3000;
-const gData = {
-  nodes: [...Array(N).keys()].map((i) => ({ id: i })),
-  links: [...Array(N).keys()]
-    .filter((id) => id)
-    .map((id) => ({
-      source: id,
-      target: Math.round(Math.random() * (id - 1))
-    }))
-};*/
-
 
 export default function FocusGraph() {
 
